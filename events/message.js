56ac162fef9a0ab6f4b5a2440e84a50dfe70f02e
jackyboi213 <52624133+jackyboi213@users.noreply.github.com>
2019-08-06 18:59:06 +0800

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -29,7 +29,7 @@ if (message.content ===('!身份組')) {message.reply
   ('你得在那個指令後面加上你要的身份組的名字。');return}
 
   var ROLEA = [roleA1,roleA2,roleA3];
-  var random_ROLEA = ROLA[Math.floor(Math.random()*ROLEA.length)];
+  var random_ROLEA = ROLEA[Math.floor(Math.random()*ROLEA.length)];
 
   if (message.content ===('!身份組 香港')){message.author.addRole(ROLE1).then(console.log)
     .catch(console.error);
