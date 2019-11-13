6fa102e6b0c17c4cb6d3d5d54486705caefac73e
jackyboi213 <52624133+jackyboi213@users.noreply.github.com>
2019-11-13 23:01:41 +0800

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,8 @@
 module.exports = (client, member) => {
     var welcome = [WELCOME1,WELCOME2,WELCOME3,WELCOME4];
-    var welcomes = welcome[Math.floor(Math.random()*WELCOME1,WELCOME2,WELCOME3,WELCOME4)];
+    var welcomes = welcome[Math.floor(Math.random()*welcome.length)];
     let ROLE = member.guild.roles.find(role => role.name === "@Member");
-    member.send
-    (welcomes).then().catch(console.error);
+    member.send(welcomes).then().catch(console.error);
     member.addRole(ROLE).then().catch(console.error);
 }
 
